e553493495e818830fcc2d063344b3ce32a77690
sabinasaloydinova <sabinasaloydinova0402@gmail.com>
2023-11-22 22:16:01 +0300

    js.alert

---
+++ js/main.js
@@ -0,0 +1,8 @@
+alert('saytga hush kelibsiz!')
+
+var name = prompt('ismingizni kiriting')
+console.log('name');
+var age = prompt('yoshingizni kiriting')
+console.log('age');
+var tel = prompt('raqamingizni kiriting')
+console.log('tel');
